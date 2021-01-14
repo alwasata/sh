4f68bc7b676a0afc3540126b48b85255698f6b4a
osamaeshmilh <osamaeshmilh@gmail.com>
2021-01-14 15:00:56 +0200

    add sidebar

--- src/main/webapp/app/core/home/home.vue
+++ src/main/webapp/app/core/home/home.vue
@@ -1,12 +1,27 @@
 <template>
     <div>
         <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
-        <b-sidebar id='sidebar-1' name='sidebar' title='Sidebar'>
+        <b-sidebar id='sidebar-1' name='sidebar' right shadow title='صحتي'>
             <div class='px-3 py-2'>
-                <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-                    in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-                </p>
+                <section>
+                    <p>sidebar</p>
+                </section>
+                <section>
+                    <h2>Section1</h2>
+                    <p>section1 whatever data you want to put in this section</p>
+                </section>
+                <section>
+                    <h2>Section2</h2>
+                    <p>section1 whatever data you want to put in this section</p>
+                </section>
+                <section>
+                    <h2>Section3</h2>
+                    <p>section1 whatever data you want to put in this section</p>
+                </section>
+                <section>
+                    <h2>Section4</h2>
+                    <p>section1 whatever data you want to put in this section</p>
+                </section>
             </div>
         </b-sidebar>
     </div>
