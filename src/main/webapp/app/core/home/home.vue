8ec4f4b812ed5b6009e536360a63d30a8126a33e
osamaeshmilh <osamaeshmilh@gmail.com>
2021-01-14 14:47:15 +0200

    add sidebar

--- src/main/webapp/app/core/home/home.vue
+++ src/main/webapp/app/core/home/home.vue
@@ -1,49 +1,14 @@
 <template>
-    <div class='home row'>
-        <div class='col-md-3'>
-            <span class='hipster img-fluid rounded'></span>
-        </div>
-        <div class='col-md-9'>
-            <h1 class='display-4'>مرحبا بكم في نظام صحتي</h1>
-            <p class='lead'>This is your homepage</p>
-
-            <div>
-                <div class='alert alert-success' v-if='authenticated'>
-                    <span v-if='username'>You are logged in as user "{{ username }}"</span>
-                </div>
-
-                <div class='alert alert-warning' v-if='!authenticated'>
-                    <span>If you want to </span>
-                    <a class='alert-link' v-on:click='openLogin()'>sign
-                        in</a><span>, you can try the default accounts:<br />- Administrator (login="admin" and password="admin") <br />- User (login="user" and password="user").</span>
-                </div>
-                <div class='alert alert-warning' v-if='!authenticated'>
-                    <span>You don't have an account yet?</span>&nbsp;
-                    <router-link class='alert-link' to='/register'>Register a new account</router-link>
-                </div>
+    <div>
+        <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
+        <b-sidebar id='sidebar-1' name='sidebar' title='Sidebar'>
+            <div class='px-3 py-2'>
+                <p>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+                    in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                </p>
             </div>
-
-            <p>
-                If you have any question on JHipster:
-            </p>
-
-            <ul>
-                <li><a href='https://www.jhipster.tech/' rel='noopener' target='_blank'>JHipster homepage</a></li>
-                <li><a href='http://stackoverflow.com/tags/jhipster/info' rel='noopener' target='_blank'>JHipster on
-                    Stack Overflow</a></li>
-                <li><a href='https://github.com/jhipster/generator-jhipster/issues?state=open' rel='noopener'
-                       target='_blank'>JHipster bug tracker</a></li>
-                <li><a href='https://gitter.im/jhipster/generator-jhipster' rel='noopener' target='_blank'>JHipster
-                    public chat room</a></li>
-                <li><a href='https://twitter.com/jhipster' rel='noopener' target='_blank'>follow @jhipster on
-                    Twitter</a></li>
-            </ul>
-
-            <p>
-                <span>If you like JHipster, don't forget to give us a star on</span> <a
-                href='https://github.com/jhipster/generator-jhipster' rel='noopener' target='_blank'>GitHub</a>!
-            </p>
-        </div>
+        </b-sidebar>
     </div>
 </template>
 
