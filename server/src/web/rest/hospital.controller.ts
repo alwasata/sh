6f9833c5c6385d07d53abfc330718a54d2190ab7
hanin ahmid <hano.hyun.hh@gmail.com>
2021-01-24 14:35:54 +0200

    Hospital -> Create User Automatically When Hospital Created

--- server/src/web/rest/hospital.controller.ts
+++ server/src/web/rest/hospital.controller.ts
@@ -7,83 +7,104 @@ import { PageRequest, Page } from '../../domain/base/pagination.entity';
 import { AuthGuard, Roles, RolesGuard, RoleType } from '../../security';
 import { HeaderUtil } from '../../client/header-util';
 import { LoggingInterceptor } from '../../client/interceptors/logging.interceptor';
+import { UserDTO } from '../../service/dto/user.dto';
+import { UserService } from '../../service/user.service';
+import { Injectable } from '@nestjs/common';
 
 @Controller('api/hospitals')
 @UseGuards(AuthGuard, RolesGuard)
 @UseInterceptors(LoggingInterceptor)
 @ApiBearerAuth()
 @ApiUseTags('hospitals')
-export class HospitalController {
-    logger = new Logger('HospitalController');
 
-    constructor(private readonly hospitalService: HospitalService) {}
+@Injectable()
+export class HospitalController{
+  logger = new Logger('HospitalController');
 
-    @Get('/')
-    @Roles(RoleType.USER)
-    @ApiResponse({
-        status: 200,
-        description: 'List all records',
-        type: HospitalDTO,
-    })
-    async getAll(@Req() req: Request): Promise<HospitalDTO[]> {
-        const pageRequest: PageRequest = new PageRequest(req.query.page, req.query.size, req.query.sort);
-        const [results, count] = await this.hospitalService.findAndCount({
-            skip: +pageRequest.page * pageRequest.size,
-            take: +pageRequest.size,
-            order: pageRequest.sort.asOrder(),
-        });
-        HeaderUtil.addPaginationHeaders(req.res, new Page(results, count, pageRequest));
-        return results;
-    }
+  // private readonly userService : UserService;
 
-    @Get('/:id')
-    @Roles(RoleType.USER)
-    @ApiResponse({
-        status: 200,
-        description: 'The found record',
-        type: HospitalDTO,
-    })
-    async getOne(@Param('id') id: string): Promise<HospitalDTO> {
-        return await this.hospitalService.findById(id);
-    }
+  constructor(private readonly hospitalService: HospitalService, private readonly userService : UserService) {}
 
-    @PostMethod('/')
-    @Roles(RoleType.ADMIN)
-    @ApiOperation({ title: 'Create hospital' })
-    @ApiResponse({
-        status: 201,
-        description: 'The record has been successfully created.',
-        type: HospitalDTO,
-    })
-    @ApiResponse({ status: 403, description: 'Forbidden.' })
-    async post(@Req() req: Request, @Body() hospitalDTO: HospitalDTO): Promise<HospitalDTO> {
-        const created = await this.hospitalService.save(hospitalDTO);
-        HeaderUtil.addEntityCreatedHeaders(req.res, 'Hospital', created.id);
-        return created;
-    }
+  @Get('/')
+  @Roles(RoleType.USER)
+  @ApiResponse({
+    status: 200,
+    description: 'List all records',
+    type: HospitalDTO,
+  })
+  async getAll(@Req() req: Request): Promise<HospitalDTO[]> {
+    const pageRequest: PageRequest = new PageRequest(req.query.page, req.query.size, req.query.sort);
+    const [results, count] = await this.hospitalService.findAndCount({
+      skip: +pageRequest.page * pageRequest.size,
+      take: +pageRequest.size,
+      order: pageRequest.sort.asOrder(),
+    });
+    HeaderUtil.addPaginationHeaders(req.res, new Page(results, count, pageRequest));
+    return results;
+  }
 
-    @Put('/')
-    @Roles(RoleType.ADMIN)
-    @ApiOperation({ title: 'Update hospital' })
-    @ApiResponse({
-        status: 200,
-        description: 'The record has been successfully updated.',
-        type: HospitalDTO,
-    })
-    async put(@Req() req: Request, @Body() hospitalDTO: HospitalDTO): Promise<HospitalDTO> {
-        HeaderUtil.addEntityCreatedHeaders(req.res, 'Hospital', hospitalDTO.id);
-        return await this.hospitalService.update(hospitalDTO);
-    }
+  @Get('/:id')
+  @Roles(RoleType.USER)
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: HospitalDTO,
+  })
+  async getOne(@Param('id') id: string): Promise<HospitalDTO> {
+    return await this.hospitalService.findById(id);
+  }
 
-    @Delete('/:id')
-    @Roles(RoleType.ADMIN)
-    @ApiOperation({ title: 'Delete hospital' })
-    @ApiResponse({
-        status: 204,
-        description: 'The record has been successfully deleted.',
-    })
-    async deleteById(@Req() req: Request, @Param('id') id: string): Promise<void> {
-        HeaderUtil.addEntityDeletedHeaders(req.res, 'Hospital', id);
-        return await this.hospitalService.deleteById(id);
-    }
+  @PostMethod('/')
+  @Roles(RoleType.ADMIN)
+  @ApiOperation({ title: 'Create hospital' })
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+    type: HospitalDTO,
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+
+  async post(@Req() req: Request, @Body() hospitalDTO: HospitalDTO): Promise<HospitalDTO> {
+
+    const userDTO =   new UserDTO();
+    userDTO.firstName   = hospitalDTO['nameAr'];
+    userDTO.lastName    = hospitalDTO['nameEn'];
+    userDTO.email       = hospitalDTO['nameEn']+'@hospital.com';
+    userDTO.login       = hospitalDTO['nameEn'];
+    userDTO.password    = hospitalDTO['nameEn'];
+    userDTO.authorities = ["ROLE_HOSPITAL_ADMIN"];
+
+    const createduser     = await this.userService.save(userDTO);
+    hospitalDTO["users"]  = [ createduser ];
+
+    const created = await this.hospitalService.save(hospitalDTO);
+    HeaderUtil.addEntityCreatedHeaders(req.res, 'Hospital', created.id);
+    
+    return created;
+  }
+
+  @Put('/')
+  @Roles(RoleType.ADMIN)
+  @ApiOperation({ title: 'Update hospital' })
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully updated.',
+    type: HospitalDTO,
+  })
+  async put(@Req() req: Request, @Body() hospitalDTO: HospitalDTO): Promise<HospitalDTO> {
+    HeaderUtil.addEntityCreatedHeaders(req.res, 'Hospital', hospitalDTO.id);
+    return await this.hospitalService.update(hospitalDTO);
+  }
+
+  @Delete('/:id')
+  @Roles(RoleType.ADMIN)
+  @ApiOperation({ title: 'Delete hospital' })
+  @ApiResponse({
+    status: 204,
+    description: 'The record has been successfully deleted.',
+  })
+  async deleteById(@Req() req: Request, @Param('id') id: string): Promise<void> {
+    HeaderUtil.addEntityDeletedHeaders(req.res, 'Hospital', id);
+    return await this.hospitalService.deleteById(id);
+  }
 }
